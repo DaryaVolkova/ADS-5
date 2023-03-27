d8d78b5195a9e561bc6f26c657776507cd28b817
Darya Volkova <125033233+DaryaVolkova@users.noreply.github.com>
2023-03-27 20:25:01 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,34 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
-class TStack {
-  // добавьте код стека
+template <class T, int size>
+class Tstack {
+private:
+    T array[size];
+    int top;
+public:
+    Tstack() :top(-1) {}
+    void push(T value) {
+        if (isFull())
+            throw std::string("Full!");
+        else
+            array[++top] = value;
+    }
+    const T& pop() {
+        if (isEmpty())
+            throw std::string("Empty!");
+        else
+            return array[top--];
+    }
+    const T& get() {
+        return array[top];
+    }
+    bool isFull() const {
+        return (top == size);
+    }
+    bool isEmpty() const {
+        return (top == -1);
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
