ab7fc3bc5751038e6951ff9c9fa03403ab116f62
Darya Volkova <125033233+DaryaVolkova@users.noreply.github.com>
2023-03-27 20:48:24 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,35 +1,36 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template <class T, int size>
 class Tstack {
-private:
-    T array[size];
-    int top;
-public:
-    Tstack() :top(-1) {}
-    void push(T value) {
-        if (isFull())
-            throw std::string("Full!");
-        else
-            array[++top] = value;
-    }
-    const T& pop() {
-        if (isEmpty())
-            throw std::string("Empty!");
-        else
-            return array[top--];
-    }
-    const T& get() {
-        return array[top];
-    }
-    bool isFull() const {
-        return (top == size);
-    }
-    bool isEmpty() const {
-        return (top == -1);
-    }
+    private:
+        T array[size];
+        int top;
+    public:
+        Tstack() :top(-1) {}
+        void push(T value) {
+            if (isFull())
+                throw std::string("Full!");
+            else
+                array[++top] = value;
+        }
+        const T& pop() {
+            if (isEmpty())
+                throw std::string("Empty!");
+            else
+                return array[top--];
+        }
+        const T& get() {
+            return array[top];
+        }
+        bool isFull() const {
+            return (top == size);
+        }
+        bool isEmpty() const {
+            return (top == -1);
+        }
 };
 
 #endif  // INCLUDE_TSTACK_H_
